210d3fc1f6423fbaedb4770d2faffa37ef28a1d6
StinaAxelsson <stina91@live.se>
2021-09-02 12:12:25 +0000

    Add print statement for the start and menu

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+print("-------------------------------------------------------")
+print("------------------------WELCOME!-----------------------")
+print("---------------This is a contact book app--------------")
+print("------Please choose what you want to do in the menu----")
+print("-------------------------------------------------------\n")
+
+
+print("MENU")
+print("1. Add a contact")
+print("2. Search for a contact")
+print("3. Delete a contact")
+print("4. Show all contacts in contact book")
+print("5. Delete all contact")
+print("6. Exit")
