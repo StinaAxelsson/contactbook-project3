6d713ff16672ffa315081778fd35546a8c6d6061
StinaAxelsson <stina91@live.se>
2021-09-02 14:03:29 +0000

    trying out a first function for the start and menu, adding a main function that holds all the runnable funtions in it

--- run.py
+++ run.py
@@ -26,19 +26,41 @@ SHEET = GSPREAD_CLIENT.open('contact_book')
 contact = SHEET.worksheet('contacts')
 data = contact.get_all_values()
 
+def start():
+    """
+    Start menu with a welcome message and a menu
+    that the user can choose between 6 different tasks.
+    """
+    print("-------------------------------------------------------")
+    print("------------------------WELCOME!-----------------------")
+    print("---------------This is a contact book app--------------")
+    print("------Please choose what you want to do in the menu----")
+    print("-------------------------------------------------------\n")
+
+def menu():
+    """
+    While loop of the menu for the user to choose what they will do,
+    and while true the program will go, and if the user want to exit the
+    program will break and start over
+    """
+    print("MENU")
+    print("1. Add a contact")
+    print("2. Search for a contact")
+    print("3. Delete a contact")
+    print("4. Show all contacts in contact book")
+    print("5. Delete all contact")
+    print("6. Exit\n") 
+
+    task = int(input("Pleaste enter the number of the task you want to do here:\n"))
+    
 
 
-print("-------------------------------------------------------")
-print("------------------------WELCOME!-----------------------")
-print("---------------This is a contact book app--------------")
-print("------Please choose what you want to do in the menu----")
-print("-------------------------------------------------------\n")
+def main():
+    """
+    contains alla the functions for the program
+    """
+    start()
+    menu()
 
 
-print("MENU")
-print("1. Add a contact")
-print("2. Search for a contact")
-print("3. Delete a contact")
-print("4. Show all contacts in contact book")
-print("5. Delete all contact")
-print("6. Exit")
+main()
